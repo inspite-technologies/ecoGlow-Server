7029d4b5cd4ff6512dd1b8fd3833560998dd6f92
INSPITE TECHNOLOGIES <developer.inspitetech@gmail.com>
2026-01-14 10:33:26 +0530

    Update packagesRoutes.js

--- routes/packagesRoutes.js
+++ routes/packagesRoutes.js
@@ -7,8 +7,9 @@ import protectAdmin from "./middleWare/adminMiddleWare.js";
 const app = express.Router();
 
 
-app.route("/").post(upload.any(), protectAdmin, createPackages);
-app.route("/").put(upload.any(), protectAdmin, updatePackages);
+app.route("/").post(protectAdmin, upload.any(), createPackages);
+app.route("/").put(protectAdmin,upload.any(),  updatePackages);
 app.route("/").get(getPackages);
+
 
 export default app;
